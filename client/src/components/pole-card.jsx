182e8992a48e9409b47cc78d78631457661a3703
unknown <mdtanvir7462@gmail.com>
2023-08-22 12:50:31 +0600

    clsx added and also add a extra component for the options

--- client/src/components/pole-card.jsx
+++ client/src/components/pole-card.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
 import { RadioGroup } from '@headlessui/react'
+import PolOption from './PolOption';
+import clsx from 'clsx';
 
-const informations =
+const INFORMATION =
 {
   _id: 1,
   question: "Chose your favorite color ?",
   discription: "This poll is crated to see what is the favorite color of Team Script Scouts ",
   options: ['red', 'green', 'blue', 'black'],
 }
-const { options, question, discription } = informations
+const { options, question, discription } = INFORMATION
 
 
 
-import React from 'react';
 
 const Polecard = () => {
   const [selected, setSelected] = useState(null)
@@ -20,17 +21,16 @@ const Polecard = () => {
 
   // Submit button fucn ==>
   const handleSubmit = () => {
-    if (selected !== null) {
+    if (!selected) {
+      return console.log('No option selected');
+    }
+
+    else {
       console.log('Selected value:', selected);
       setSelected(null);
-    } else {
-      console.log('No option selected');
     }
   }
 
-
-  //for disabled the button if there is no option selected===>
-  const isSubmitDisabled = selected === null;
 
 
 
@@ -41,56 +41,13 @@ const Polecard = () => {
       <div className="mx-auto w-full max-w-2xl rounded-md bg-[#1f1f38]   p-[5%] ">
         <h1 className='mb-6 font-semibold text-center text-white text-md md:text-2xl '>{question}</h1>
 
-        {/* options */}
         <RadioGroup value={selected} onChange={setSelected}>
 
           <div className="space-y-2">
-            {options?.map((singleOption) => (
-              <RadioGroup.Option
+            {options?.map((singleOption) =>
 
-                key={singleOption}
-                value={singleOption}
-                className={({ active, checked }) =>
-                  `${active
-                    ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-sky-300'
-                    : ''
-                  }
-        ${checked ? 'bg-sky-900  bg-opacity-75 text-white' : 'bg-white'
-                  }
-          relative flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none`
-                }
-              >
-                {({ active, checked }) => (
-                  <>
-                    <div className="flex items-center justify-between w-full">
-                      <div className="flex items-center">
-                        <div className="text-sm">
-                          <RadioGroup.Label
-                            as="p"
-                            className={`font-medium  ${checked ? 'text-white' : 'text-gray-900'
-                              }`}
-                          >
-                            {singleOption}
-                          </RadioGroup.Label>
-                          <RadioGroup.Description
-                            as="span"
-                            className={`inline ${checked ? 'text-sky-100' : 'text-gray-500'
-                              }`}
-                          >
-
-                          </RadioGroup.Description>
-                        </div>
-                      </div>
-                      {checked && (
-                        <div className="text-white shrink-0">
-                          <CheckIcon className="w-6 h-6" />
-                        </div>
-                      )}
-                    </div>
-                  </>
-                )}
-              </RadioGroup.Option>
-            ))}
+              <PolOption singleOption={singleOption} key={singleOption}></PolOption>
+            )}
           </div>
         </RadioGroup>
 
@@ -100,14 +57,14 @@ const Polecard = () => {
           <button
             onClick={handleSubmit}
             type='submit'
-            disabled={isSubmitDisabled}
-            className={`rounded mt-3 md:mt-5 px-3 md:px-5 py-1.5 md:py-2.5 overflow-hidden group bg-green-500 relative hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300`}
-            style={isSubmitDisabled ? { cursor: 'not-allowed' } : {}}
+            disabled={!selected}
+            className={clsx(
+              "rounded mt-3 md:mt-5 px-3 md:px-5 py-1.5 md:py-2.5 overflow-hidden group bg-green-500 relative hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300",
+              !selected && 'cursor-not-allowed', // add cursor class conditionally
+            )}
           >
-            <span className={`absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease ${isSubmitDisabled
-              ? 'opacity-0' // Hide the mouse when button is disabled
-              : ''
-              }`}></span>
+            <span className='absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease '></span>
+
             <span className="relative">SUBMIT</span>
           </button>
         </span>
@@ -121,18 +78,4 @@ const Polecard = () => {
 
 export default Polecard;
 
-function CheckIcon(props) {
-  return (
-    <svg viewBox="0 0 24 24" fill="none" {...props}>
-      <circle cx={12} cy={12} r={12} fill="#fff" opacity="0.2" />
-      <path
-        d="M7 13l3 3 7-7"
-        stroke="#fff"
-        strokeWidth={1.5}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  )
-}
 
