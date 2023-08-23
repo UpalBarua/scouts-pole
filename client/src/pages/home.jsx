02ca6168446222109438bfb2f71920f99313e20e
Upal Barua <upalbarua52@gmail.com>
2023-08-23 15:18:51 +0600

    did some refactoring

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,8 +1,9 @@
-
 const HomePage = () => {
-  return <div>
-    <h3 className="text-center">This is our home page</h3>
-  </div>
+  return (
+    <div>
+      <h3 className="text-center">This is our home page</h3>
+    </div>
+  );
 };
 
 export default HomePage;
