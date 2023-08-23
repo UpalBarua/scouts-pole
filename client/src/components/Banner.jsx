b423985bffe737ac6dc77596b75326ae9ffcc156
Upal Barua <upalbarua52@gmail.com>
2023-08-23 13:57:59 +0600

    Did some refactoring

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,23 +1,22 @@
-import { Link } from "react-router-dom";
-import banner from "../assets/banner.jpg";
+import { Link } from 'react-router-dom';
+import banner from '../assets/banner.jpg';
+
 const Banner = () => {
   return (
-    <div className="grid md:grid-cols-2 grid-cols-1 gap-3 justify-center items-center h-96 py-12 px-8 md:px-2">
+    <div className="grid grid-cols-1 gap-3 justify-center items-center px-8 py-12 h-96 md:grid-cols-2 md:px-2">
       <div className="mb-4">
-        <h3 className="text-sm sm:text-5xl font-bold">Be part of Decison</h3>
-        <p className="py-3 text-sm md:text-lg font-medium">
+        <h3 className="text-sm font-bold sm:text-5xl">Be part of Decison</h3>
+        <p className="py-3 text-sm font-medium md:text-lg">
           Secure and Anonymous Polling. Write your opinion . Poll from anywhere.
         </p>
         <Link
           to="/about"
-          className="hover:bg-purple-600 hover:text-white py-1 px-6 mt-3 font-medium text-sm border border-gray-500 text-gray-700"
-        >
+          className="px-6 py-1 mt-3 text-sm font-medium text-gray-700 border border-gray-500 hover:bg-purple-600 hover:text-white">
           About Us
         </Link>
         <Link
           to="/about"
-          className="hover:bg-purple-600 hover:text-white py-1 px-6 mt-3 font-medium text-sm border border-gray-500 text-gray-700 ml-2"
-        >
+          className="px-6 py-1 mt-3 ml-2 text-sm font-medium text-gray-700 border border-gray-500 hover:bg-purple-600 hover:text-white">
           Vote
         </Link>
       </div>
