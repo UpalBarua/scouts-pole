644b54cbd02c45820c8f25531df7216d6e5ba44b
Upal Barua <upalbarua52@gmail.com>
2023-08-28 13:35:32 +0600

    redesigned the auth-page, added react-hot-toast

--- client/src/layouts/root-layout.jsx
+++ client/src/layouts/root-layout.jsx
@@ -1,24 +1,12 @@
 import { Outlet } from 'react-router-dom';
-import Navbar from '../components/navbar';
+import Navbar from '../components/navbar/navbar';
 import Footer from '../components/footer';
 
 const RootLayout = () => {
   return (
     <>
       <Navbar />
-      {/* <p className="text-primary-50">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-        nihil sit earum odio eum libero fugiat labore sint deleniti incidunt!
-      </p>
-      <p className="text-primary-100">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-        nihil sit earum odio eum libero fugiat labore sint deleniti incidunt!
-      </p>
-      <p className="text-white">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-        nihil sit earum odio eum libero fugiat labore sint deleniti incidunt!
-      </p> */}
-      {/* <Outlet /> */}
+      <Outlet />
       {/* <Footer /> */}
     </>
   );
