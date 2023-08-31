aa4f7d09c563ceb39fd459afb7e2b30c8fe9c83e
Ador Zamaan <adorzamaan@gmail.com>
2023-08-31 23:04:24 +0600

    footer refactoring

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,21 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 const footer_menu = [
   {
-    title: 'Home',
-    link: '/',
+    title: "Home",
+    link: "/",
   },
   {
-    title: 'About Us',
-    link: '/',
+    title: "About Us",
+    link: "/",
   },
   {
-    title: 'Our Team',
-    link: '/',
+    title: "History",
+    link: "/history",
   },
   {
-    title: 'History',
-    link: '/history',
+    title: "Contact Us",
+    link: "/",
+  },
+];
+
+const contributors = [
+  {
+    title: "Imran Hasan Ovi",
+    profile: "https://avatars.githubusercontent.com/u/121815437?v=4",
+    path: "https://github.com/GitByImran",
+  },
+  {
+    title: "Upal Barua",
+    profile: "https://avatars.githubusercontent.com/u/56833544?v=4",
+    path: "https://github.com/UpalBarua",
+  },
+  {
+    title: "Ador",
+    profile: "https://avatars.githubusercontent.com/u/84873174?v=4",
+    path: "https://github.com/jamanador",
+  },
+  {
+    title: "Niaz Abir",
+    profile: "https://avatars.githubusercontent.com/u/103358513?v=4",
+    path: "https://github.com/niaz-abir",
+  },
+  {
+    title: "Tajul Islam Tanvir",
+    profile: "https://avatars.githubusercontent.com/u/121679880?v=4",
+    path: "https://github.com/niaz-abir",
+  },
+  {
+    title: "Amran Hossen",
+    profile: "https://avatars.githubusercontent.com/u/105711432?v=4",
+    path: "https://github.com/AJAmran",
   },
 ];
 
@@ -37,40 +70,23 @@ const Footer = () => {
             </li>
           ))}
         </ul>
-        <div className="flex justify-center py-3">
-          <Link to="/" className="mx-3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              className="fill-current">
-              <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-            </svg>
-          </Link>
-          <Link to="/" className="mx-3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              className="fill-current">
-              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-            </svg>
-          </Link>
-          <Link to="/" className="mx-3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              className="fill-current">
-              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-            </svg>
-          </Link>
-        </div>
+        <h3 className="text-center text-lg font-semibold pt-3">
+          Development Team
+        </h3>
+        <ul className="flex justify-center pt-3 pb-6">
+          {contributors?.map(({ title, profile, path }, i) => (
+            <a href={path} target="_blank" key={i}>
+              <img
+                className="w-10 rounded-full border border-slate-500 shadow-sm mx-3"
+                src={profile}
+                alt=""
+                title={title}
+              />
+            </a>
+          ))}
+        </ul>
         <p>
-          Copyright © 2023 - All right reserved by Team{' '}
+          Copyright © 2023 - All right reserved by Team{" "}
           <Link to="/" className="font-bold text-green-500">
             Script Scouts
           </Link>
